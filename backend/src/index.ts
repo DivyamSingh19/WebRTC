9593e0987046ea323ca9734b8a7f662ef8effe26
Divyam Singh <divyamsingh1918@gmail.com>
2025-01-24 12:30:42 +0530

    updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-import WebSocket, { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port:8080});
+
+wss.on('connection',function connection(ws){
+   
+    ws.on('message',function message(data:any){
+        const message = JSON.parse(data);
+        console.log(message)
+    });
+    
+})
